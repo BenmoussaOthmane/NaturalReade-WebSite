3ddde374820efcd3630ee6df71e434c383c56012
CoderX <CoderXG@gmail.com>
2020-12-03 12:24:15 +0200

    grid Layout to List

--- lib/screen/list.dart
+++ lib/screen/list.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Liste extends StatefulWidget {
   @override
@@ -8,22 +9,80 @@ class Liste extends StatefulWidget {
 }
 
 class _ListeState extends State<Liste> {
+  var jsonResponse;
+  Future<String>_loadFromAsset() async {
+  return await rootBundle.loadString("assets/datav.json");
+}
+
+Future parseJson() async {
+    String jsonString = await _loadFromAsset();
+    jsonResponse = jsonDecode(jsonString);
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.only(top:50.0,left: 50,right: 50.0),
         child:FutureBuilder(
           future: DefaultAssetBundle.of(context).loadString("assets/datav.json"),
           builder: (context,snapshot){
             var mydata = json.decode(snapshot.data.toString());
-            return ListView.builder(
-              itemBuilder: (BuildContext contex,int index){
-                return Text(
-                  mydata[index]["name"],
+            if(!snapshot.hasData){
+              return Center(
+                child: Text(
+                  'Loiding'
+                ),
+              );
+            }
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 250,crossAxisSpacing: 2.0,mainAxisSpacing: 2.0,childAspectRatio: 2.0),
+                  itemBuilder: (context,index){
+                    return Container(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              color: Colors.red,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    mydata[index]['title'],
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                    )
+                                  ),
+                                  Text(
+                                    mydata[index]['title'],
+                                    style: TextStyle(
+                                      color: Colors.grey
+                                    ),
+                                  ),
+                                  Text(
+                                    mydata[index]['title'],
+                                    style: TextStyle(
+                                      color: Colors.amber
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount:mydata.length,
                 );
-              },
-              itemCount:mydata.length,
-            );
           },
         )
       ),
